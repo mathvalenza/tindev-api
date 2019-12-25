3d45da0e6db5cc0e5d89838b5a7091e82b0d94e1
Valenza <matheusvvalenza@gmail.com>
2019-12-25 01:07:29 -0300

    created first version of api: user model and list, like and dislike controllers

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,27 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user);
+    let targetDev = null
+
+    try {
+      targetDev = await Dev.findById(devId);
+    } catch (error) {
+      return res.status(400).json({ error: 'Dev not exists' });
+    }
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log('deu match');
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
