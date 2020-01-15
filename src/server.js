8ca40ac39ab3426c9071cd1d506a57b9d1176515
Matheus Vinícius Valenza <mvalenza@fesctecnologia.com.br>
2020-01-15 16:05:46 -0300

    fixed unhandled warning

--- src/server.js
+++ src/server.js
@@ -7,10 +7,13 @@ const routes = require('./routes');
 const server = express();
 
 mongoose.connect('mongodb+srv://user:user@cluster0-4er0j.mongodb.net/tindev?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true });
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    server.use(cors());
+    server.use(express.json());
+    server.use(routes);
+    
+    server.listen(3333);
+  })
+  .catch((e) => console.error(e));
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
-
-server.listen(3333);
